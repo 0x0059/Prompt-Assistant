e1929f5f651fddf7d2676e94b6876a23f1f7d9a0
0x0059 <Zeeearo@duck.com>
2025-04-23 19:43:49 +0800

    优化提示词管理模块，提升可读性及可可扩展性

---
+++ packages/core/src/services/template/templates/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 模板内容索引文件
+ */
+
+export * from './general-optimize';
+export * from './output-format-optimize';
+export * from './advanced-optimize';
+export * from './iterate';
+export * from './structured-format'; 
